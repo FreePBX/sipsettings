7158a2aaf80eace5b0d8e7fd4ad0436ab522f6d0
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 15:56:43 -0500

    new license tags

--- page.sipsettings.php
+++ page.sipsettings.php
@@ -1,24 +1,6 @@
 <?php /* $Id:$ */
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 
-// Original Release by Philippe Lindheimer
-// Copyright Philippe Lindheimer (2009)
-// Copyright Bandwidth.com (2009)
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
   /* Determines how many columns per row for the codecs and formats the table */
   $cols_per_row   = 4;
   $width          = (100.0 / $cols_per_row);
