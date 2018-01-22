88a44677be50f6160211280cdb7bccb4a127d98f
Franck Danard <fdanard@sangoma.com>
2018-01-22 18:21:01 +0100

    Fix Patterns for subnet mask.

--- general.page.php
+++ general.page.php
@@ -144,7 +144,7 @@ foreach ($tlsowners as $chan => $txt) {
 	foreach ($localnetstmp as $id => $arr) {
 		$lnhtm .= '<div class = "lnet form-group form-inline" data-nextid='.($id+1).'>';
 		$lnhtm .= '	<input type="text" name="localnets['.$id.'][net]" class="form-control localnet network validate-ip"  pattern="^$|^([0-9]{1,3}\.){3}[0-9]{1,3}$" value="'.$arr['net'].'">/';
-		$lnhtm .= '	<input type="text" name="localnets['.$id.'][mask]" class="form-control netmask cidr validate-netmask" pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$" value="'.$arr['mask'].'">';
+		$lnhtm .= '	<input type="text" name="localnets['.$id.'][mask]" class="form-control netmask cidr validate-netmask" pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$|^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$" value="'.$arr['mask'].'">';
 		$lnhtm .= '</div>';
 	}
 	?>
@@ -161,7 +161,7 @@ foreach ($tlsowners as $chan => $txt) {
 						<div class="col-md-9">
 							<div class = "lnet form-group form-inline" data-nextid=1>
 								<input type="text" name="localnets[0][net]" class="form-control localnet network validate-ip"  pattern="^$|^([0-9]{1,3}\.){3}[0-9]{1,3}$" value="<?php echo isset($localnets[0]['net']) ? $localnets[0]['net'] : '' ?>"> /
-								<input type="text" name="localnets[0][mask]" class="form-control netmask cidr validate-netmask" pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$" value="<?php echo isset($localnets[0]['mask']) ? $localnets[0]['mask'] : ''?>">
+								<input type="text" name="localnets[0][mask]" class="form-control netmask cidr validate-netmask" pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$|^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$" value="<?php echo isset($localnets[0]['mask']) ? $localnets[0]['mask'] : ''?>">
 							</div>
 							<?php echo $lnhtm?>
 							<input type="button" id="localnet-add" value="<?php echo $add_local_network_field ?>" />
@@ -391,7 +391,7 @@ foreach ($tlsowners as $chan => $txt) {
 						<div class="col-md-9">
 							<?php $i = 0;foreach($ice_blacklist as $can) {?>
 								<div class="form-group form-inline">
-									<input type="hidden" id="ice_blacklist_count" name="ice_blacklist_count[]" value="<?php echo $i?>"><input type="text" id="ice_blacklist_ip_<?php echo $i?>" name="ice_blacklist_ip_<?php echo $i?>" class="form-control ice-blacklist" pattern="^$|^([0-9]{1,3}\.){3}[0-9]{1,3}$" value="<?php echo $can['address']?>"> / <input type="text" id="ice_blacklist_subnet_<?php echo $i?>" name="ice_blacklist_subnet_<?php echo $i?>" class="form-control"  pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$" value="<?php echo $can['subnet']?>">
+									<input type="hidden" id="ice_blacklist_count" name="ice_blacklist_count[]" value="<?php echo $i?>"><input type="text" id="ice_blacklist_ip_<?php echo $i?>" name="ice_blacklist_ip_<?php echo $i?>" class="form-control ice-blacklist" pattern="^$|^([0-9]{1,3}\.){3}[0-9]{1,3}$" value="<?php echo $can['address']?>"> / <input type="text" id="ice_blacklist_subnet_<?php echo $i?>" name="ice_blacklist_subnet_<?php echo $i?>" class="form-control"  pattern="^$|^((?:[1-9])|(?:[1-2][0-9])|(?:3[0-2]))$|^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$" value="<?php echo $can['subnet']?>">
 								</div>
 							<?php $i++;} ?>
 							<div id="ice-blacklist-buttons">
