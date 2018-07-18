c0fb0c01ccb307b80a7e620e4cbb6ba1632586af
James Finstrom <jfinstrom@sangoma.com>
2018-07-18 23:05:26 +0000

    Remove uninstall file, restore cleanup

--- Restore.php
+++ Restore.php
@@ -3,10 +3,10 @@ namespace FreePBX\modules\Sipsettings;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $settings = $this->getConfigs();
+    $settings = reset($this->getConfigs());
     foreach ($settings['kvstore'] as $key => $value) {
         $this->FreePBX->Sipsettings->setMultiConfig($value, $key);
     }
-    $this->FreePBX->Sipsettings->loadDbConfig($settings['database']);
+    $this->FreePBX->Sipsettings->loadDbConfigs($settings['database']);
   }
 }
--- uninstall.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* $Id:$ */
-if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-
-sql("DELETE FROM kvstore WHERE module = 'sipsettings'");
-sql("DROP TABLE `sipsettings`");
