4b54f18f24dcd3d5d95d4afcbc3ea15f6fc6f23e
Andrew Nagy <andrew.nagy@the159.com>
2019-02-11 13:13:44 -0500

    [Module Tag script: sipsettings 15.0.6.1]

--- Restore.php
+++ Restore.php
@@ -3,7 +3,7 @@ namespace FreePBX\modules\Sipsettings;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
-    $settings = reset($this->getConfigs());
+    $settings = $this->getConfigs();
     foreach ($settings['kvstore'] as $key => $value) {
         $this->FreePBX->Sipsettings->setMultiConfig($value, $key);
     }
