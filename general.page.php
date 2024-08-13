aee59e7152b2b3d7d1b0c8fc8d3bb351fe8f6f42
kapil gupta <kgupta@sangoma.com>
2024-08-12 23:41:49 -0400

    Update links
    This PR updates the links to the old wiki (wiki.freepbx.org) to the new location (sangomakb.atlassian.net) and updates the settings 'more-info' in the module.xml file
    
    Bugfix FreePBX/issue-tracker#700

--- general.page.php
+++ general.page.php
@@ -414,7 +414,7 @@ foreach ($tlsowners as $chan => $txt) {
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<span id="stunaddr-help" class="help-block fpbx-help-block"><?php echo _("Hostname or address for the STUN server used when determining the external IP address and port an RTP session can be reached at. The port number is optional. If omitted the default value of 3478 will be used. This option is blank by default. (A list of STUN servers: http://wiki.freepbx.org/x/YQCUAg)")?></span>
+				<span id="stunaddr-help" class="help-block fpbx-help-block"><?php echo _("Hostname or address for the STUN server used when determining the external IP address and port an RTP session can be reached at. The port number is optional. If omitted the default value of 3478 will be used. This option is blank by default. (A list of STUN servers: https://sangomakb.atlassian.net/wiki/spaces/PG/pages/25824149/PBX+GUI+-+STUN+Servers)")?></span>
 			</div>
 		</div>
 	</div>
@@ -599,7 +599,7 @@ foreach ($tlsowners as $chan => $txt) {
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<span id="webrtcstunaddr-help" class="help-block fpbx-help-block"><?php echo _("Hostname or address for the STUN server used when determining the external IP address and port an RTP session can be reached at. The port number is optional. If omitted the default value of 3478 will be used. This option is blank by default. (A list of STUN servers: http://wiki.freepbx.org/x/YQCUAg)")?></span>
+				<span id="webrtcstunaddr-help" class="help-block fpbx-help-block"><?php echo _("Hostname or address for the STUN server used when determining the external IP address and port an RTP session can be reached at. The port number is optional. If omitted the default value of 3478 will be used. This option is blank by default. (A list of STUN servers: https://sangomakb.atlassian.net/wiki/spaces/PG/pages/25824149/PBX+GUI+-+STUN+Servers)")?></span>
 			</div>
 		</div>
 	</div>
